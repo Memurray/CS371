3447ccc050a8e633e436340f8c240c84dfee0110
Memurray <39600016+Memurray@users.noreply.github.com>
2019-04-06 17:14:30 -0400

    Update stop logic
    Will keep running until top flow has reached packet count goal.

--- scapy-skeleton.py
+++ scapy-skeleton.py
@@ -6,21 +6,21 @@ import socket
 import os
 import csv    
 
-c = 0
-MAX_READS = 100
-timeDict = dict()
-data = dict()
-
 #Basic global + 1 operation
 def increment():
     global c
     c = c+1
 
 #Define rules for when sniffer stops
+#This method was a very simple way to stop run when highest flow hits a threshold without checking highest flow after every read
 def stopfilter(x):
      global c
-     if c == MAX_READS:
-         return True
+     if c == MAX_READS:  #If counter is at threshold 
+         max= getBestFlowCount()  
+         if max == MAX_READS:  #check highest flow count
+            return True  #if highest flow is at threshold, end
+         else:
+            c = max  # set counter to highest flow count
      else:
          return False
 
@@ -39,7 +39,9 @@ def updateTuple(old,new):
     out1 = counter+1
     out2 = (old[1]*counter + new[0])/(counter+1.0)
     out3 = (old[2]*counter + new[1])/(counter+1.0)
-    return (out1,out2,out3)   
+    out4 = (old[3]*counter + new[2])/(counter+1.0)
+    out5 = (old[4]*counter + new[3])/(counter+1.0)
+    return (out1,out2,out3,out4,out5)   
 
 #Looks through data, finding flow with highest occurance count  
 def getBestFlow():
@@ -49,8 +51,16 @@ def getBestFlow():
         if value[0] > max:
             max = value[0]
             bestkey = key          
-    print("Highest Flow: ",bestkey, " Count: ",data[bestkey]) 
+    #print("Highest Flow: ",bestkey, " Data: ",data[bestkey]) 
     return bestkey
+
+def getBestFlowCount():
+    max = 0
+    bestkey = ()
+    for key, value in data.items():
+        if value[0] > max:
+            max = value[0]          
+    return max
 
 #Flips input flow and returns information if it exists
 def getPairFlow(flow):
@@ -62,11 +72,15 @@ def getPairFlow(flow):
     for key, value in data.items():
         print(key, "\t Count:",data[key])
 
+def printToFile(flow,features,label):
+    with open('eval.csv','a',newline='') as eval:        #open csv in append mode
+        eval_writer = csv.writer(eval, delimiter=',')    #setup line writing
+        eval_writer.writerow([flow,features[1],features[2],features[3],features[4],label])  #write this data to csv
+        
 
 def fields_extraction(x):         #each loop for sniff do this
     global dict
     if IP in x:               #if IP is a valid request, do this
-        #eval_writer.writerow([x[IP].src,x[IP].dst,x[IP].len,x.time])       #write this data to csv
         increment()  
         n=()
         if TCP in x:
@@ -75,15 +89,17 @@ def fields_extraction(x):         #each loop for sniff do this
             n = (x[IP].src, x[UDP].sport,x[IP].dst,x[UDP].dport,"udp")         #Generate flow id for UDP  
 
         if n in data:
-            data[n] = updateTuple(data[n],(x[IP].len,checkTime(x.time,n)))  #Update the feature averages
+            data[n] = updateTuple(data[n],(x[IP].len,checkTime(x.time,n),x[IP].ttl,x[IP].frag))  #Update the feature averages
         else:
-            data[n] = (1,x[IP].len,checkTime(x.time,n))  #Set initial feature values
-            
+            data[n] = (1,x[IP].len,checkTime(x.time,n),x[IP].ttl,x[IP].frag)  #Set initial feature values           
 
                 
-with open('eval.csv','a',newline='') as eval:                #open csv in append mode
-    eval_writer = csv.writer(eval, delimiter=',')            #setup line writing
-    
-sniff(prn = fields_extraction,stop_filter = stopfilter)
-best = getBestFlow()
-getPairFlow(best)
+for i in range(25):
+    c = 0
+    MAX_READS = 1000
+    timeDict = dict()
+    data = dict()   
+    sniff(prn = fields_extraction,stop_filter = stopfilter)
+    best = getBestFlow()
+    #getPairFlow(best)
+    printToFile(best,data[best],4)
