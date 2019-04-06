480c338dfaaa953b99e4170c6988fea925976c08
Memurray <39600016+Memurray@users.noreply.github.com>
2019-04-06 17:12:19 -0400

    Add support for all data output

--- ML-skeleton.py
+++ ML-skeleton.py
@@ -2,19 +2,21 @@ import pandas as pd
 import numpy as np
 import csv 
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
 from sklearn import tree
 
 
-
-df = pd.read_csv("data.csv", header=None)
+df = pd.read_csv("eval.csv", header=None)
 # You might not need this next line if you do not care about losing information about flow_id etc. All you actually need to
 # feed your machine learning model are features and output label.
-columns_list = ['flow_id', 'IPsrc', 'IPdst', 'proto', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5', 'label']
+columns_list = ['flow_id','feature_1', 'feature_2', 'feature_3', 'feature_4', 'label']
 df.columns = columns_list
-features = ['proto', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5']
+features = ['feature_1', 'feature_2', 'feature_3', 'feature_4']
 
 X = df[features]
 y = df['label']
@@ -28,8 +30,8 @@ for i in range(0, 10):
     clf.fit(X_train, y_train)
 
     # Neural network (MultiPerceptron Classifier)
-    # clf = MLPClassifier()
-    # clf.fit(X_train, y_train)
+    #clf = MLPClassifier()
+    #clf.fit(X_train, y_train)
 
     #SVM's
     # clf = SVC(gamma='auto')     #SVC USE THIS
@@ -38,4 +40,22 @@ for i in range(0, 10):
 
 
     #here you are supposed to calculate the evaluation measures indicated in the project proposal (accuracy, F-score etc)
-    result = clf.score(X_test, y_test)  #accuracy score
+    y_pred = clf.predict(X_test)
+    y_score = clf.score(X_test, y_test)
+    print('Accuracy: ', y_score)
+
+    # Compute the average precision score
+    micro_precision = precision_score(y_pred, y_test, average='micro')
+    print('Micro-averaged precision score: {0:0.2f}'.format(
+          micro_precision))
+
+    macro_precision = precision_score(y_pred, y_test, average='macro')
+    print('Macro-averaged precision score: {0:0.2f}'.format(
+          macro_precision))
+
+    per_class_precision = precision_score(y_pred, y_test, average=None)
+    print('Per-class precision score:', per_class_precision)
+
+    print('F1 score:',f1_score(y_test, y_pred, average='macro'))
+    print()     
+          
